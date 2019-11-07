eb50bda2b27003eb4cbe6bdd5de8d10efed65b80
dimazhuravlev <dmitryzhur@gmail.com>
2019-11-07 13:18:09 +0300

    add metatag to pinterest

--- src/features/pictures/index.tsx
+++ src/features/pictures/index.tsx
@@ -5,11 +5,11 @@ import FasterCase from "./organisms/fasterCase";
 import WazzappsCase from "./organisms/wazzappsCase";
 import KamyshevCase from "./organisms/kamyshevCase";
 import styles from "./pictures.module.css";
-// import EnumberCase from "./organisms/enumberCase";
+import EnumberCase from "./organisms/enumberCase";
 
 const Pictures = () => (
   <section id="paneContainer" className={styles.pictures}>
-    {/* <EnumberCase /> */}
+    <EnumberCase />
     <KamyshevCase />
     <FasterCase />
     <BreadheadCase />
